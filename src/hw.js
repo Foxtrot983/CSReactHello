3129ba6f7fc37b0cffa295dc6927e89cf11bf988
Foxtrot983 <65424445+Foxtrot983@users.noreply.github.com>
2022-03-26 13:59:43 +0300

    Initial commit

---
+++ src/hw.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./hw.css";
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="HW">
+        <h1>HelloWorld</h1>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HelloWorld from "./hw";
+import "./index.css";
+
+ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
